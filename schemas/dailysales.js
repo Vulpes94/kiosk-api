72c7eb1dbafdb59b73616ba97fca90e372e23cd0
RegistryROK <registry.forbusiness@gmail.com>
2021-07-04 01:22:30 +0900

    rename

---
+++ schemas/dailysales.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+
+const {Schema} = mongoose;
+
+const dailySales = new Schema(
+  {
+    menu_name: {
+      type: String,
+      required: true,
+      unique: true,
+      ref: 'Menu',
+    },
+    sales_quantity: {
+      type: Number,
+      required: true,
+    },
+    menu_price: {
+      type: Number,
+      required: true,
+      ref: 'Menu',
+    },
+    total_price: {
+      type: Number,
+      required: true,
+    },
+  },
+  {
+    versionKey: false,
+  }
+);
+
+module.exports = mongoose.model('dailysales', dailySales);
---
+++ schemas/menu_mgnt.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+
+const {Schema} = mongoose;
+
+const menuMgnt = new Schema(
+  {
+    menu_name: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    menu_price: {
+      type: Number,
+      required: true,
+    },
+    menu_stock: {
+      type: Number,
+      required: true,
+    },
+  },
+  {
+    versionKey: false,
+  }
+);
+
+module.exports = mongoose.model('menu_mgnt', menuMgnt);
