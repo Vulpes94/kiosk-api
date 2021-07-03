0d05d655c0a330a8fa6799248bef1967955705de
RegistryROK <registry.forbusiness@gmail.com>
2021-07-04 01:21:55 +0900

    add: new schemas

---
+++ schemas/menu_slct.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+
+const {Schema} = mongoose;
+
+const menuSlct = new Schema(
+  {
+    menu_name: {
+      type: String,
+      required: true,
+      unique: true,
+      ref: 'Menu',
+    },
+    menu_price: {
+      type: Number,
+      required: true,
+    },
+    menu_stock: {
+      type: Number,
+      required: true,
+    },
+  },
+  {
+    versionKey: false,
+  }
+);
+
+module.exports = mongoose.model('menu_slct', menuSlct);
---
+++ schemas/wishlist.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+
+const {Schema} = mongoose;
+
+const wishList = new Schema(
+  {
+    table_no: {
+      type: Number,
+      required: true,
+      ref: 'Table',
+    },
+    menu_name: {
+      type: String,
+      required: true,
+      ref: 'Menu',
+    },
+    menu_price: {
+      type: Number,
+      required: true,
+    },
+    order_quantity: {
+      type: Number,
+      required: true,
+    },
+  },
+  {
+    versionKey: false,
+  }
+);
+
+module.exports = mongoose.model('wishlist', wishList);
