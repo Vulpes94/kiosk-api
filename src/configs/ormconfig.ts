1d71d649e65090ac2e354976e88e2d97bddb1203
Vulpes94 <kjunfi94@gmail.com>
2022-06-28 12:40:16 +0900

    chore: DB Migration MySQL to MariaDB

--- src/configs/ormconfig.ts
+++ src/configs/ormconfig.ts
@@ -2,7 +2,7 @@ import { ConnectionOptions } from 'typeorm';
 require('dotenv').config();
 
 const ORMConfig: ConnectionOptions = {
-  type: 'mysql',
+  type: 'mariadb',
   host: 'localhost',
   port: 3306,
   username: process.env.DB_USERNAME,
@@ -10,6 +10,7 @@ const ORMConfig: ConnectionOptions = {
   database: 'kible',
   synchronize: true,
   logging: process.env.NODE_ENV === 'production' ? false : true,
+  timezone: '+09:00',
   entities:
     process.env.NODE_ENV === 'production' ? ['build/entities/**/*.js'] : ['src/entities/**/*.ts'],
   migrations:
